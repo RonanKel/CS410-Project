dd16338e578fb4ee732e62cc3d31860803966a3e
MaxH403 <mxherms403@gmail.com>
2024-06-05 01:48:48 -0700

    New Build and press Tab to pause

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && isPausable) {
+        if (Input.GetKeyDown(KeyCode.Tab) && isPausable) {
             if (isPaused) {
                 UnpauseGame();
             }
